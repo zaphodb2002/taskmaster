68c9cb6b74f6626eba8168b7a0db9129ed0cdbba
Joshua Lindhardt <zaphodb2002@gmail.com>
2023-10-02 13:52:32 -0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+
+/////////////////////////
+/// TaskMaster
+///////////////////
+///
+/// TaskMaster is a tool to manage TaskWarrior and conform it to an opinionated workflow.///
+/// Features
+/// ========
+/// - Consume TaskWarrior data and produce high quality reports
+/// - Enforce conformity and improve usability of tasks and workflows via rules
+/// - Create a file structure rather than a single flat file with 2 way interoperability
+/// - Enhance and/or replace TaskWarrior's handling of recurring tasks
+///
+/// Reporting
+/// =========
+/// - Recurring task completion percent
+///     - Lowest performing
+///     - Highest performing
+///     - by project and individually
+///     - by timespan, for comparison
+/// - Scheduling Assistance
+///     - recurring tasks by day of week and time of day
+///     - visual calendar layout
+/// Rules
+/// =====
+///
+
+fn main(){
+    println!("Hello World");
+}
+
